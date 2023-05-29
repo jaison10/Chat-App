3c0ac6c14510652224a8728211930c2cce33d8bd
jaison10 <jaisonj1010@gmail.com>
2023-05-30 00:11:00 +0530

    username in display

--- src/index.js
+++ src/index.js
@@ -23,9 +23,9 @@ io.on('connection', (socket) => {
         if(error){
             return callback(error)
         }
-        socket.join(user.room)
-        socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+        socket.join(user.room) 
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
         callback()
         // socket.emit - sends to current user
         // io.emit - sends to all
@@ -42,21 +42,21 @@ io.on('connection', (socket) => {
         }
         const user = getUser(socket.id)
 
-        io.to(user.room).emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback()
     })
 
     socket.on('sendLocation', (coords, callback) => {
         const user = getUser(socket.id)
-
-        io.to(user.room).emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        console.log("found user ", user);
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username,`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
     socket.on('disconnect', () => {
         const user = removeUser(socket.id)
         if(user){
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
         }
     })
 })
